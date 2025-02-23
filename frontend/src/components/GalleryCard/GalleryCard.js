731614c904bdf56454a3885aada21f2e80d275a9
hz270593nas <nehoda.artem@a-bank.com.ua>
2025-02-23 20:15:00 +0200

    Add spiner when photo loading

--- frontend/src/components/GalleryCard/GalleryCard.js
+++ frontend/src/components/GalleryCard/GalleryCard.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
+import { FaSpinner } from 'react-icons/fa';
 import './GalleryCard.css';
 
 const GalleryCard = ({ download_url, author }) => {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="gallery-card">
-      <img src={download_url} alt="empty" />
-      <p>Autor: {author}</p>
+      <img
+        src={download_url}
+        alt="empty"
+        onLoad={(e) => {
+          setLoading(false);
+        }}
+      />
+      <div
+        className="caption"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        {loading ? <FaSpinner className="spinner" /> : <p>Autor: {author}</p>}
+      </div>
     </div>
   );
 };
