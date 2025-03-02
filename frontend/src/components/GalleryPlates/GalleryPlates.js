9e2ea90bb38b102631bec8d87d82bcb1f043f250
hz270593nas <nehoda.artem@a-bank.com.ua>
2025-03-02 22:16:38 +0200

    Redisign gallery with react-photo-album lib

--- frontend/src/components/GalleryPlates/GalleryPlates.js
+++ frontend/src/components/GalleryPlates/GalleryPlates.js
@@ -1,29 +1,82 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { RowsPhotoAlbum } from 'react-photo-album';
+import 'react-photo-album/rows.css';
 import { selectPhotos } from '../../redux/slices/photoSlice.js';
 import { selectOnlyFavouriteFilter } from '../../redux/slices/filterSlice.js';
-import Row from 'react-bootstrap/Row';
+import { setImgToModal } from '../../redux/slices/modalSlice';
+import { BsBookmarkHeart, BsBookmarkHeartFill, BsXLg } from 'react-icons/bs';
+import { toggleFavourite, deletePhoto } from '../../redux/slices/photoSlice';
 
-import GalleryCard from '../GalleryCard/GalleryCard';
 import './GalleryPlates.css';
 const GalleryPlates = () => {
+  const dispatch = useDispatch();
   const photos = useSelector(selectPhotos); // array with photos
   const onlyFavouriteFilter = useSelector(selectOnlyFavouriteFilter); //sign only favourite filter
+
+  const handleShow = ({ src, title }) => {
+    dispatch(setImgToModal({ image: src, author: title }));
+  };
 
   //filtered array with photos
   const filteredPhotos = photos.filter((photo) => {
     return onlyFavouriteFilter ? photo.isFavourite : true;
   });
+  const formattedPhotos = filteredPhotos.map((photo) => ({
+    ...photo,
+    src: photo.download_url, // або photo.imageUrl
+    title: photo.author,
+  }));
+
+  const handleCheckIsFavourite = ({ id }) => {
+    dispatch(toggleFavourite(id));
+  };
+  const deleteCardFromPlate = ({ id }) => {
+    dispatch(deletePhoto(id));
+  };
 
   return (
     <div className="gallery-plates">
-      {filteredPhotos.length ? (
-        <Row xs={1} md={2} lg={4}>
-          {filteredPhotos.map((photo) => {
-            return <GalleryCard key={photo.id} {...photo} />;
-          })}
-        </Row>
+      {formattedPhotos.length ? (
+        <RowsPhotoAlbum
+          photos={formattedPhotos}
+          render={{
+            // render image selection icon
+            extras: (t, { photo }) => {
+              return (
+                <>
+                  <span
+                    style={{ position: 'absolute', top: 5, left: 5 }}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleCheckIsFavourite(photo);
+                    }}
+                  >
+                    {photo.isFavourite ? (
+                      <BsBookmarkHeartFill className="like-icon-filled" />
+                    ) : (
+                      <BsBookmarkHeart className="like-icon-unfilled" />
+                    )}
+                  </span>
+                  <span>
+                    <BsXLg
+                      className="close-card-icon"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteCardFromPlate(photo);
+                      }}
+                    />
+                  </span>
+                </>
+              );
+            },
+          }}
+          onClick={({ event, photo }) => {
+            event.preventDefault();
+            handleShow(photo);
+          }}
+        />
       ) : (
-        <h4>No added photos yet!</h4>
+        <h4 style={{ textAlign: 'center' }}>No added photos yet!</h4>
       )}
     </div>
   );
